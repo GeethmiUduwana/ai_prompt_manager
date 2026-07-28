37b375594b76557116d255e299a5485c3b46a796
Geethmi Uduwana <it23653740@my.sliit.lk>
2026-07-28 16:51:06 +0530

    fix render build - remove npm, fix config caching, add runtime migrate

--- scripts/render-build.sh
+++ scripts/render-build.sh
@@ -14,10 +14,7 @@ if [ ! -f /tmp/database.sqlite ]; then
   touch /tmp/database.sqlite
 fi
 
-npm install
-npm run production
 composer install --no-dev --optimize-autoloader --no-interaction
-php artisan migrate --force
-php artisan config:cache
+
 php artisan route:cache
 php artisan view:cache
